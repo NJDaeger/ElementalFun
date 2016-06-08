bc1cb2dbee79c9719241090e8e2ee7fcba59bb73
NJDaeger <noahdaeger@hotmail.com>
2016-06-08 13:58:18 -0400

    Added a bit more information on the commands.

--- src/com/njdaeger/elef/ElementalFun.java
+++ src/com/njdaeger/elef/ElementalFun.java
@@ -17,18 +17,18 @@ public class ElementalFun extends JavaPlugin{
 	
 	/*
 	 * command idea list: (add to it pls)
-	 * groundbreaker
-	 * catlitter
+	 * groundbreaker (i can attempt to do this one -nj)
+	 * catlitter (i need to think this thru more)
 	 * fart
-	 * particles
-	 * halp
-	 * funsound
-	 * animalhat
-	 * fireworks
-	 * rainbow
-	 * drunk
-	 * doublejump
-	 * trap
+	 * particles (this will be a challenge)
+	 * halo (halo of particles around the players head)
+	 * funsound (ye what it is)
+	 * animalhat (ye what it is)
+	 * fireworks (ye what it is)
+	 * rainbow (name in rainbow colors)
+	 * drunk (cool FOV effects)
+	 * doublejump (ye what it is)
+	 * trap (enclose the player in webs)
 	 * funnydeath
 	 * levitate
 	 */
